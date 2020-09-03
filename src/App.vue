bf7d3338820a7d2b11f8dfcdcdeacb3e82bd4c56
Rama Pradana <ramapradana67@hotmail.com>
2020-09-03 12:33:04 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="sandbox">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      app
+      overflow
+    >
+      <v-list>
+        <v-list-item-group v-model="item" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-row style="padding:24px">
+        <v-col cols="12" md="12">
+          <span>Scheme</span>
+          <v-switch v-model="$vuetify.theme.dark" primary label="Dark"></v-switch>
+        </v-col>
+      </v-row>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="primaryDrawer.clipped" app>
+      <v-app-bar-nav-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Cafe Momon Candidasa</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="10">
+            <v-card>
+              <v-card-text>
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <span>Scheme</span>
+                    <v-switch v-model="$vuetify.theme.dark" primary label="Dark"></v-switch>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <span>Drawer</span>
+                    <v-radio-group v-model="primaryDrawer.type" column>
+                      <v-radio
+                        v-for="drawer in drawers"
+                        :key="drawer"
+                        :label="drawer"
+                        :value="drawer.toLowerCase()"
+                        primary
+                      ></v-radio>
+                    </v-radio-group>
+                    <v-switch v-model="primaryDrawer.clipped" label="Clipped" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.floating" label="Floating" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.mini" label="Mini" primary></v-switch>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <span>Footer</span>
+                    <v-switch v-model="footer.inset" label="Inset" primary></v-switch>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn text>Cancel</v-btn>
+                <v-btn text color="primary">Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer :inset="footer.inset" app>
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    drawers: ["Default (no property)", "Permanent", "Temporary"],
+    items: [
+      { text: "Kasir", icon: "mdi-folder" },
+      { text: "Riwayat Transaksi", icon: "mdi-account-multiple" },
+    ],
+    primaryDrawer: {
+      model: null,
+      type: "temporary",
+      clipped: false,
+      floating: false,
+      mini: false,
+    },
+    footer: {
+      inset: false,
+    },
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
